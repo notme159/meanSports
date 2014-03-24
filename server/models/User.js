7d10b738ac689d05f688088c2c4f168180c13fb7
Jan Azaltovic <notme159@gmail.com>
2014-03-24 21:04:39 +0100

    first, basic auth, fixed toastr at login, salt at update user

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose'),
+  encrypt = require('../utilities/encryption');
+
+var userSchema = mongoose.Schema({
+  firstName: {type:String, required:'{PATH} is required!'},
+  lastName: {type:String, required:'{PATH} is required!'},
+  username: {
+    type: String,
+    required: '{PATH} is required!',
+    unique:true
+  },
+  salt: {type:String, required:'{PATH} is required!'},
+  hashed_pwd: {type:String, required:'{PATH} is required!'},
+  roles: [String]
+});
+userSchema.methods = {
+  authenticate: function(passwordToMatch) {
+    return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+  },
+  hasRole: function(role) {
+    return this.roles.indexOf(role) > -1;
+  }
+};
+var User = mongoose.model('User', userSchema);
+
+function createDefaultUsers() {
+  User.find({}).exec(function(err, collection) {
+    if(collection.length === 0) {
+      var salt, hash;
+      salt = encrypt.createSalt();
+      hash = encrypt.hashPwd(salt, 'john@gmail.com');
+      User.create({firstName:'John',lastName:'Doe',username:'john@gmail.com', salt: salt, hashed_pwd: hash, roles: ['admin']});
+      salt = encrypt.createSalt();
+      hash = encrypt.hashPwd(salt, 'jenna@gmail.com');
+      User.create({firstName:'Jenna',lastName:'Sousa',username:'jenna@gmail.com', salt: salt, hashed_pwd: hash, roles: []});
+      salt = encrypt.createSalt();
+      hash = encrypt.hashPwd(salt, 'jojo@gmail.com');
+      User.create({firstName:'Joj',lastName:'Koll',username:'joj@gmail.com', salt: salt, hashed_pwd: hash});
+    }
+  })
+};
+
+exports.createDefaultUsers = createDefaultUsers;
