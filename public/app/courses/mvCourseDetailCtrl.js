22e3e8965fc21ca17faf9d89d3f9a97a29a36e35
Jan Azaltovic <notme159@gmail.com>
2014-03-25 13:44:49 +0100

    caching off rdy to add notes

--- public/app/courses/mvCourseDetailCtrl.js
+++ public/app/courses/mvCourseDetailCtrl.js
@@ -1,9 +1,3 @@
-angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCachedCourses, $routeParams) {
-  mvCachedCourses.query().$promise.then(function(collection) {
-    collection.forEach(function(course) {
-      if(course._id === $routeParams.id) {
-        $scope.course = course;
-      }
-    });
-  });
+angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCourse, $routeParams) {
+  $scope.course = mvCourse.get({_id: $routeParams.id});
 });
